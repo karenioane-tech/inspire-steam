f14a4f7e541012c2c9bf79146a084aa541249d90
karenioane-tech <karenmwaniki1@gmail.com>
2026-02-11 09:18:57 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,8 @@
+university= "TUM"
+grade="B+"
+favorite_course= "kuccps decision"
+cluster_points=79
+print(f"my favorite university is {university} ")
+print(f"my grade is {grade} ")
+print(f"my favorite course is {favorite_course}")
+print(f"my cluster points is {cluster_points} ")
---
+++ python-basics/hello.py
@@ -0,0 +1,12 @@
+#This is a comment
+print("hello from Karen")
+#my city
+print("i am from Kigali")
+#my hobbies
+print("i like to swimming,adrenaline activities and travelling")
+age=17
+height= 154
+weight= 65
+print(f"i am {age} years old")
+print(f"i am {height} cemtimeters")
+print(f"i am {weight} kgs")
