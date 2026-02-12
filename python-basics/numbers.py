7d1e729b58ebe6c22b70829b3e3b98a7c4156c9f
karenioane-tech <karenmwaniki1@gmail.com>
2026-02-12 10:41:30 +0300

    Add basic programs for user input and arithmetic operations
    Introduces introductory scripts for collecting user input and performing basic arithmetic, supporting learning and demonstration of Python fundamentals.

---
+++ python-basics/numbers.py
@@ -0,0 +1,17 @@
+first_number=60
+second_number=30
+
+#program to add, subtract, divide, multiply numbers
+
+first_number=int(input("enter the first_number"))
+second_number=int(input("enter the second_number"))
+
+sum_numbers=first_number+second_number
+difference_numbers=first_number-second_number
+quotient_numbers=first_number/second_number
+product_numbers=first_number*second_number
+
+print(f"sum_numbers={sum_numbers}")
+print(f"difference_numbers={difference_numbers}")
+print(f"quotient_numbers={quotient_numbers}")
+print(f"product_numbers={product_numbers}")
---
+++ python-basics/user_input.py
@@ -0,0 +1,9 @@
+#Name:Karen Mwaniki
+
+#Date:11/02/2026
+
+#Program to get user_input
+
+name=input("what is your name ")
+
+print("name")
