df214f7e2ac19fbac1483d8ecbab1a42b45d1816
karenioane-tech <karenmwaniki1@gmail.com>
2026-02-13 08:20:22 +0300

    added user_input.py,vnumbers.py and string_formatting.py

--- python-basics/numbers.py
+++ python-basics/numbers.py
@@ -1,17 +1,22 @@
-first_number=60
-second_number=30
+#first_number=60
+#second_number=30
 
-#program to add, subtract, divide, multiply numbers
+#first_number=int(input("what is the first number"))
+#second_number=int(input("what is the second number"))
+#sum_numbers=first_number+second_number
+#difference_numbers=first_number-second_number
+#quotient_numbers=first_number/second_number
+#product_numbers=first_number*second_number
 
-first_number=int(input("enter the first_number"))
-second_number=int(input("enter the second_number"))
+#print("the sum is", first_number + second_number)
+#print("the difference is", first_number - second_number)
+#print("the quotient is", first_number / second_number)
+#print("the product is", first_number * second_number)
 
-sum_numbers=first_number+second_number
-difference_numbers=first_number-second_number
-quotient_numbers=first_number/second_number
-product_numbers=first_number*second_number
+third_number=int(input("what is the third number"))
 
-print(f"sum_numbers={sum_numbers}")
-print(f"difference_numbers={difference_numbers}")
-print(f"quotient_numbers={quotient_numbers}")
-print(f"product_numbers={product_numbers}")
+print(f"the type of the third number is {type(third_number)}")
+#convert third number to an integer
+third_number=int(third_number)
+
+print(f"the type of the third number is {type(third_number)}")
---
+++ python-basics/string_formatting.py
@@ -0,0 +1,54 @@
+#Name:Karen Mwaniki
+#Date: 12/02/2026
+#String Formatting
+
+#get string length
+sentence = "I am learning python"
+
+string_length = len(sentence)
+
+print(f"The length is:{string_length}")
+
+#spliting a string
+sentence_2= "Mathematics Physics"
+split=sentence_2.split(" ")
+
+print(f"The first subject is: {split[1]}")
+
+
+#make everything uppercase
+code= "kdg34kbrkdq"
+
+capitalized=code.upper()
+
+print(f"New code is: {capitalized}")
+
+#make everything lowercase
+code_2= "KDg34kbrkdq"
+
+lowercase=code_2.lower()
+
+print(f"New code is: {lowercase}")
+
+#replace characters in a string
+
+balance= "100Kes"
+amount_added= "50Kes"
+
+cleaned_balance= balance.replace("Kes","")
+
+print(f"Cleaned balance is: {cleaned_balance}")
+
+cleaned_amount_added= amount_added.replace("Kes","")
+
+print(f"Cleaned amount added is: {cleaned_amount_added}")
+
+#
+new_balance= int(cleaned_balance) + int(cleaned_amount_added)
+
+print(f"New balance is: {new_balance}")
+
+new_balance_string= str(new_balance - 90)
+
+print(f"New balance after deduction is: {new_balance_string}")
+
