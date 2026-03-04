73ada69ea722212f41fd414270656f4cf7df6fe5
karenioane-tech <karenmwaniki1@gmail.com>
2026-03-04 22:21:07 +0300

    "add buttto.py,leds.py,diagram.json,main.py,picozero.py,ssd1306.py,wokwi-project.txt under iot"

---
+++ iot/leds.py
@@ -0,0 +1,20 @@
+#Name:Karen Mwaniki
+#Date:18/02/2026
+#Program to 
+
+
+from machine import Pin
+import time
+
+
+red_led=Pin(28, Pin.OUT)
+yellow_led=Pin(27,Pin.OUT)
+while True :
+    red_led.on()
+    yellow_led.off()
+    time.sleep(1) # Wait for USB to become ready
+    red_led.off()
+    yellow_led.on()
+    time.sleep(1)
+
+    print("Learning IOT")
