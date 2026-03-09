ff0b7748302d52dc560928453a1f3fa6e479f0c8
karenioane-tech <karenmwaniki1@gmail.com>
2026-03-09 19:58:05 +0300

    "add manual_control.py"

--- Drones/manual_control.py
+++ Drones/manual_control.py
@@ -1,30 +1,39 @@
-from pysimverse import drone
-import cv2
+from pysimverse import Drone
 import time
+import cv2 
+import cvzone
+drone = Drone()
 
-drone= drone()
-drone.connect()
-time.sleep(1)
+drone.take_off() # Distance is in cm
+time.sleep(2)
 
-drone.take_off(5)
-rc_speed=250
-
-if 
+drone.take_off(5) # Distance is in cm
+rc_speed = 250
 
 while True:
-    key=cv2.waitKey(1)& 0xff
+    key = cv2.waitKey(1) & 0xFF
 
-#get all values to 0
-    left_right=0
-    forward_backward=0
-    up_down=0
-    yaw=0
+#get all values 0
+    left_right = 0
+    foward_back = 0
+    up_down = 0
+    yaw = 0
 
-        left_right, forward_backward, up_down, yaw = new_func(rc_speed, key)
-    
-    break
-
-drone.send_rc_control(left_right,
-                      forward_backward,
-                      up_down,
-                      yaw)
+    if key == ord('w'):
+        foward_back = rc_speed
+    elif key == ord('s'):
+        foward_back = -rc_speed
+    elif key == ord('a'):
+        left_right = -rc_speed  
+    elif key == ord('d'):
+        left_right = rc_speed
+    elif key == ord('q'):
+        yaw = -1
+    elif key == ord('e'):
+        yaw = 1
+    elif key == ord('r'):
+        up_down = rc_speed
+    elif key == ord('f'):
+        up_down = -rc_speed
+        break
+    drone.send_rc_control(left_right, foward_back, up_down, yaw)
